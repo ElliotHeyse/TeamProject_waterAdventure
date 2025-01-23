4a07f0e9dd52bd35b7b6138c66357e3892affeaf
ElliotHeyse <elliot.heyse@student.howest.be>
2025-01-23 10:47:48 +0100

    feat: ✨ add custom fonts 'Inter' and 'Sniglet' to enhance typography

--- webapp/tailwind.config.ts
+++ webapp/tailwind.config.ts
@@ -55,7 +55,9 @@ const config: Config = {
 				sm: "calc(var(--radius) - 4px)"
 			},
 			fontFamily: {
-				sans: [...fontFamily.sans]
+				sans: [...fontFamily.sans],
+				inter: ['Inter', ...fontFamily.sans],
+				sniglet: ['Sniglet', ...fontFamily.sans]
 			}
 		}
 	},
